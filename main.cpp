b046129945ad05213831dc927ccd7639f69176fc
Howard <hschang.2008@gmail.com>
2018-05-15 16:33:14 +0800

    Added simple calculation code

--- main.cpp
+++ main.cpp
@@ -6,7 +6,12 @@ using namespace Output;
 
 int main()
 {
+	int a = 10, b = 12;
+	int c = a + b;
+
 	printMessage("Hello World");
 	printMessage("Added a new line here");
+
+	printMessage("The result is " + std::to_string(c));
 	return 0;
 }
